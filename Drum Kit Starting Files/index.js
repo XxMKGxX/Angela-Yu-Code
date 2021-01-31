1009871051b5b0374fb02e70fd9a0f713d0f9f36
XxMKGxX <conradkandoro@gmail.com>
2021-01-31 07:12:01 +0200

    Three updated txt files, js and css

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,1 +1,8 @@
-alert("Hello")
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++)
+
+    document.querySelectorAll(".drum")[i].addEventListener("click", handleClick)
+
+function handleClick() {
+    var audio = new Audio('sounds/crash.mp3');
+    audio.play();
+}
