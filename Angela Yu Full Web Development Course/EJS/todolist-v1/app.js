855f35b13c825416a43528ac57a217a5ed90df92
XxMKGxX <conradkandoro@gmail.com>
2021-02-15 21:26:50 +0200

    Challenge fixes.

--- Angela Yu Full Web Development Course/EJS/todolist-v1/app.js
+++ Angela Yu Full Web Development Course/EJS/todolist-v1/app.js
@@ -3,43 +3,33 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+var items = [
+    'caleb is great'
+];
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.set('view engine', 'ejs')
 
 
 app.get("/", function(req, res) {
     var today = new Date();
-    var currentDay = today.getDay();
-    var day = "";
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
 
-    switch (currentDay) {
-        case 0:
-            day = "Sunday"
-            break;
-        case 1:
-            day = "Monday"
-            break;
-        case 2:
-            day = "Tuesday"
-            break;
-        case 3:
-            day = "Wednesday"
-            break;
-        case 4:
-            day = "Thursday"
-            break;
-        case 5:
-            day = "Friday"
-            break;
-        case 6:
-            day = "Saturday"
-            break;
+    var day = today.toLocaleDateString("en-US", options)
 
-        default:
-            break;
-    }
-
-    res.render("list", { kindOfDay: day }) //kindOfDay is the var name and day is the value of that var based on the if else statement
+    res.render("list", { kindOfDay: day, newListItems: items }) //kindOfDay is the var name and day is the value of that var based on the if else statement
 });
+
+app.post("/", function(request, response) {
+    var item = request.body.newItem;
+    items.push(item);
+    response.redirect("/");
+})
 
 app.listen(3000, function(req, res) {
     console.log("Server is running at port 3000")
