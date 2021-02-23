77dbff6b5536408a1994df5d7f42f28057adb81b
XxMKGxX <conradkandoro@gmail.com>
2021-02-23 09:51:36 +0200

    Challenge done

--- Angela Yu Full Web Development Course/Blog Website Challenge/ejs-challenge/app.js
+++ Angela Yu Full Web Development Course/Blog Website Challenge/ejs-challenge/app.js
@@ -9,17 +9,46 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
+let posts = [];
+
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+app.get("/", function(req, res) {
+    res.render("home.ejs", {
+        startingContent: homeStartingContent,
+        posts: posts
+    });
+});
 
+app.get("/contact", function(req, res) {
+    res.render("contact.ejs", {
+        cContent: contactContent
+    });
+});
 
+app.get("/about", function(req, res) {
+    res.render("about.ejs", {
+        aContent: aboutContent
+    });
+});
 
+app.get("/compose", function(req, res) {
+    res.render("compose.ejs")
+});
 
+app.post("/compose", function(req, res) {
+    const post = {
+        title: req.body.publish_Title,
+        content: req.body.publish_Body
+    };
+    posts.push(post)
+    res.redirect("/");
 
+});
 
 
 
@@ -29,5 +58,5 @@ app.use(express.static("public"));
 
 
 app.listen(3000, function() {
-  console.log("Server started on port 3000");
+    console.log("Server started on port 3000");
 });
